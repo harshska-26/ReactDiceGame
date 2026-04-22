50a6bc0086f7664d590804495c8a1f90b28de7bb
harshka-26 <harshithbabu@criskasecurity.com>
2026-04-22 15:18:55 +0530

    Hard Mode Changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { ScoreComp } from "./components/Score.Compnents/Score.component";
 import { Topline } from "./components/Topline.Components/Topline.component";
 import { ButtonGroup } from "./layouts/ButtonGroup.layout";
 import { diceControlBtns } from "./metadata/buttons.metadata";
-import { EditPlyrComp, Popup } from "./layouts/Popup.layout";
+import { EditPlyrComp, HardmodePopup, Popup } from "./layouts/Popup.layout";
 
 const App = () => {
   const [randomNumbers, SetrandomNumbers] = useState([1, 1]);
@@ -19,8 +19,8 @@ const App = () => {
   const [rulesOpen, setRulesOpen] = useState(false);
   const [epOpen, setepOpen] = useState(false);
   const [playerNames, SetplayerNames] = useState(["PLAYER ONE", "PLAYER TWO"]);
-  const [hardMode, setHardMode] = useState(false)
-  const [lastRollSix, setlastRollSix] = useState(false)
+  const [hardMode, setHardMode] = useState(false);
+  const [lastRollSix, setlastRollSix] = useState(false);
 
   const newGameFunc = () => {
     SetCurrentScore(0);
@@ -31,139 +31,157 @@ const App = () => {
   };
 
   const hardModeFunc = () => {
-    const updatedPlayerNames = [...playerNames]
-      SetplayerNames(updatedPlayerNames)
-    setHardMode(!hardMode)
+    const updatedPlayerNames = [...playerNames];
+    SetplayerNames(updatedPlayerNames);
+    setHardMode(!hardMode);
     newGameFunc();
-  }
+  };
+
+  const nrmlModeFunc = () => {
+    const updatedPlayerNames = [...playerNames];
+    SetplayerNames(updatedPlayerNames);
+    setHardMode(!hardMode);
+    newGameFunc();
+  };
 
   const RollDice = () => {
     const randomNumone = Math.ceil(Math.random() * 6);
     const randomNumtwo = Math.ceil(Math.random() * 6);
     const randomNum = randomNumone + randomNumtwo;
     SetrandomNumbers([randomNumone, randomNumtwo]);
-      if (hardMode) {
-        if (randomNumone === 6 || randomNumtwo === 6) {
-          if (lastRollSix === true) {
-            SettotalScore((prev) => {
-              const newScore = [...prev];
-              newScore[activeplayer] = Math.max(0, newScore[activeplayer] - 10);
-              return newScore;
-            });
-            SetCurrentScore(0)
-            Setactiveplayer(activeplayer === 0 ? 1 : 0)
-            setlastRollSix(false);
-          } else {
-            setlastRollSix(true)
-            SetCurrentScore((prev) => prev + randomNum)
-          }
+    if (hardMode) {
+      if (randomNumone === 6 || randomNumtwo === 6) {
+        if (lastRollSix === true) {
+          SettotalScore((prev) => {
+            const newScore = [...prev];
+            newScore[activeplayer] = Math.max(0, newScore[activeplayer] - 10);
+            return newScore;
+          });
+          SetCurrentScore(0);
+          Setactiveplayer(activeplayer === 0 ? 1 : 0);
+          setlastRollSix(false);
         } else {
           if (randomNumone === 1 || randomNumtwo === 1) {
             SetCurrentScore(0);
             Setactiveplayer(activeplayer === 0 ? 1 : 0);
-          }
-          else {
-            setlastRollSix(false);
-            SetCurrentScore((prev) => prev + randomNum)
+          } else {
+            setlastRollSix(true);
+            SetCurrentScore((prev) => prev + randomNum);
           }
         }
       } else {
         if (randomNumone === 1 || randomNumtwo === 1) {
           SetCurrentScore(0);
           Setactiveplayer(activeplayer === 0 ? 1 : 0);
-          setlastRollSix(false);
         } else {
-          SetCurrentScore((prev) => prev + randomNum);
           setlastRollSix(false);
+          SetCurrentScore((prev) => prev + randomNum);
         }
       }
-    }
-  
-
-
-    const HoldFunc = () => {
-      const updatedScores = [...totalScore];
-      activeplayer === 0
-        ? (updatedScores[0] += CurrentScore)
-        : (updatedScores[1] += CurrentScore);
-      SettotalScore(updatedScores);
-      SetCurrentScore(0);
-      activeplayer === 0 ? Setactiveplayer(1) : Setactiveplayer(0);
-    };
-
-    const InputFunc = (e) => {
-      console.log(`Winning Score Set to ${e.target.value}`);
-      SetWinningScore(e.target.value);
-    };
-
-    const RulesDialogFunc = () => {
-      setRulesOpen(!rulesOpen);
-    };
-
-    const Epfunc = () => {
-      SetplayerNames(["PLAYER ONE", "PLAYER TWO"])
-      setepOpen(!epOpen);
-    };
-
-
-    const EditplayerFunc = (e) => {
-      let targetedElement = e.target;
-      if (targetedElement.id === "inputone") {
-        SetplayerNames([targetedElement.value, playerNames[1]]);
+    } else {
+      if (randomNumone === 1 || randomNumtwo === 1) {
+        SetCurrentScore(0);
+        Setactiveplayer(activeplayer === 0 ? 1 : 0);
+        setlastRollSix(false);
       } else {
-        SetplayerNames([playerNames[0], targetedElement.value]);
+        SetCurrentScore((prev) => prev + randomNum);
+        setlastRollSix(false);
       }
-    };
-
-    const epEnterFunc = () => {
-      const updatedPlayerNames = [...playerNames]
-      SetplayerNames(updatedPlayerNames)
-      setepOpen(false)
     }
-
-    const funcObj = {
-      rollDiceBtn: RollDice,
-      holdBtn: HoldFunc,
-      rulesModeBtn: RulesDialogFunc,
-      editPlayerNameBtn: Epfunc,
-      hardModeBtn: hardModeFunc,
-    };
-
-    let ActiveBg = activeplayer ? "sideone" : "sideone bgOne";
-    let ActiveBgtwo = activeplayer ? "sideone bgOne" : "sideone";
-    let UiChange = hardMode ? "root bodyBG" : "root";
-
-
-    return (
-      <div className={UiChange}>
-        <div className="entirebox">
-          <div className={ActiveBg}>
-            <PlayerComp
-              activeplayer={activeplayer === 0}
-              totalscore={totalScore[0]}
-              name={playerNames[0]}
-            />
-            <ScoreComp CurrentScore={activeplayer === 0 ? CurrentScore : 0} />
-          </div>
-          <div className={ActiveBgtwo}>
-            <PlayerComp
-              activeplayer={activeplayer === 1}
-              totalscore={totalScore[1]}
-              name={playerNames[1]}
-            />
-            <ScoreComp CurrentScore={activeplayer === 1 ? CurrentScore : 0} />
-          </div>
-          <Topline onClick={newGameFunc} />
-          <ImageComp randomNums={randomNumbers} />
-          <div className="dicecontrols">
-            <ButtonGroup funcObj={funcObj} dataset={diceControlBtns} />
-          </div>
-          <BottomLine funcObj={funcObj} onChange={InputFunc} />
-          {rulesOpen && <Popup closeRules={RulesDialogFunc} />}
-          {epOpen && <EditPlyrComp onEnter={epEnterFunc} onChange={EditplayerFunc} onClose={Epfunc} />}
-        </div>
-      </div>
-    );
   };
 
-  export default App;
+  const HoldFunc = () => {
+    const updatedScores = [...totalScore];
+    activeplayer === 0
+      ? (updatedScores[0] += CurrentScore)
+      : (updatedScores[1] += CurrentScore);
+    SettotalScore(updatedScores);
+    updatedScores[activeplayer] >= WinningScore && newGameFunc();
+    SetCurrentScore(0);
+    activeplayer === 0 ? Setactiveplayer(1) : Setactiveplayer(0);
+  };
+
+  const InputFunc = (e) => {
+    console.log(`Winning Score Set to ${e.target.value}`);
+    SetWinningScore(e.target.value);
+  };
+
+  const RulesDialogFunc = () => {
+    setRulesOpen(!rulesOpen);
+  };
+
+  const Epfunc = () => {
+    SetplayerNames(["PLAYER ONE", "PLAYER TWO"]);
+    setepOpen(!epOpen);
+  };
+
+  const EditplayerFunc = (e) => {
+    let targetedElement = e.target;
+    if (targetedElement.id === "inputone") {
+      SetplayerNames([targetedElement.value, playerNames[1]]);
+    } else {
+      SetplayerNames([playerNames[0], targetedElement.value]);
+    }
+  };
+
+  const epEnterFunc = () => {
+    const updatedPlayerNames = [...playerNames];
+    SetplayerNames(updatedPlayerNames);
+    setepOpen(false);
+  };
+
+  const funcObj = {
+    rollDiceBtn: RollDice,
+    holdBtn: HoldFunc,
+    rulesModeBtn: RulesDialogFunc,
+    editPlayerNameBtn: Epfunc,
+    hardModeBtn: hardModeFunc,
+    normalModeBtn: nrmlModeFunc,
+  };
+
+  let ActiveBg = `sideone ${activeplayer === 0 ? (hardMode ? "hardmode" : "bgOne") : ""}`;
+  let ActiveBgtwo = `sideone ${activeplayer === 1 ? (hardMode ? "hardmode" : "bgOne") : ""}`;
+  let UiChange = hardMode ? "root bodyBG" : "root";
+
+  return (
+    <div className={UiChange}>
+      <div className="entirebox">
+        <div className={ActiveBg}>
+          <PlayerComp
+            activeplayer={activeplayer === 0}
+            totalscore={totalScore[0]}
+            name={playerNames[0]}
+          />
+          <ScoreComp CurrentScore={activeplayer === 0 ? CurrentScore : 0} />
+        </div>
+        <div className={ActiveBgtwo}>
+          <PlayerComp
+            activeplayer={activeplayer === 1}
+            totalscore={totalScore[1]}
+            name={playerNames[1]}
+          />
+          <ScoreComp CurrentScore={activeplayer === 1 ? CurrentScore : 0} />
+        </div>
+        <Topline onClick={newGameFunc} />
+        <ImageComp randomNums={randomNumbers} />
+        <div className="dicecontrols">
+          <ButtonGroup funcObj={funcObj} dataset={diceControlBtns} />
+        </div>
+        <BottomLine funcObj={funcObj} onChange={InputFunc} />
+        {!hardMode && rulesOpen && <Popup closeRules={RulesDialogFunc} />}
+        {hardMode && rulesOpen && (
+          <HardmodePopup closeRules={RulesDialogFunc} />
+        )}
+        {epOpen && (
+          <EditPlyrComp
+            onEnter={epEnterFunc}
+            onChange={EditplayerFunc}
+            onClose={Epfunc}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/layouts/Popup.layout.jsx
+++ src/layouts/Popup.layout.jsx
@@ -11,11 +11,60 @@ export const Popup = ({ closeRules }) => {
             RULES
           </strong>
         </h1>
-        <h3>first Rule</h3>
-        <h3>Second Rule</h3>
-        <h3>Third Rule</h3>
-        <h3>fourth Rule</h3>
-        <h3>fifth Rule</h3>
+        <ol>
+          <li>The game has 2 players,playing in rounds.</li>
+          <li>
+            In each turn,a player rolls a dice as many times as he whishes. Each
+            result get added to his ROUND score.
+          </li>
+          <li>
+            BUT, if the player rolls a 1, all from his Round score gets
+            lost.After that, it's the next player's turn.
+          </li>
+          <li>
+            The player can choose to "HOLD", which means that his ROUND score
+            gets added to his GLOBAL score. After that, it's the next player's
+            turn.
+          </li>
+          <li>
+            The first player to reach 50 points GLOBAL score wins the game.
+          </li>
+          <li>
+            you can change the predefined score of 50 in the "Set Target Score"
+            input box "GOOD LUCK BUDDY !!!"
+          </li>
+        </ol>
+        <button className="close-button" onClick={closeRules}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export const HardmodePopup = ({ closeRules }) => {
+  return (
+    <div className="overlay">
+      <div className="Dialog" align="center">
+        <h1 align="center">
+          <strong>
+            <FcRules />
+            RULES
+          </strong>
+        </h1>
+        <li>Including the rules before here are some New Rules.</li>
+        <li>
+          Player looses 20 points his ENTIRE score when he rolls a 6 in a row.
+          After that, it's the next player's turn.
+        </li>
+        <li>
+          The Global score is increased to 100 points.so first player reaching
+          GLOBAL score wins the game.
+        </li>
+        <li>
+          Another dice is added to make the game faster to reach GLOBAL SCORE
+          and interesting.GOOD LUCK BUDDY !!!
+        </li>
         <button className="close-button" onClick={closeRules}>
           Close
         </button>
