3438152b5ad6dea6704d814f2d54ea3c98cafd97
harshka-26 <harshithbabu@criskasecurity.com>
2026-04-23 12:06:31 +0530

    hardmode playername Update

--- src/components/Playerline.Components/Player.component.jsx
+++ src/components/Playerline.Components/Player.component.jsx
@@ -1,11 +1,12 @@
 import "./Player.component.css";
 
-export const PlayerComp = ({ activeplayer, name, totalscore }) => {
+export const PlayerComp = ({ hardMode, activeplayer, name, totalscore }) => {
   let ActiveStat = activeplayer ? "playerBox active" : "playerBox";
+  let HardmodeStat = activeplayer ? `player-name ${hardMode ? "hardmodeBox" : ""} active` : `player-name`;
 
   return (
     <div className={ActiveStat}>
-      <h2 className="player-name">{name}</h2>
+      <h2 className={HardmodeStat}>{name}</h2>
       <p>{totalscore}</p>
     </div>
   );
